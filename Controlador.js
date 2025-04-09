7af249599fb52746cef80204ad96ceaf39ad2d57
pulidxxx <andresfelipepulidosuarez@gmail.com>
2025-04-08 20:46:20 -0500

    Add AVL and Red-Black Tree visualization functionality
    - Implemented AVL tree creation, insertion, and deletion functions.
    - Added Red-Black tree creation, insertion, and deletion functions.
    - Developed a user interface for inputting nodes and visualizing trees.
    - Included random node generation for both tree types.
    - Enhanced README with application features and technologies used.
    - Added application visualization image.

---
+++ Controlador.js
@@ -0,0 +1,385 @@
+// matriz de entrada
+var array;
+
+// -------------ARBOLES AVL---------------
+
+/**
+ * CREACION ARBOL AVL
+ *
+ * Se define la raiz con altura nula
+ */
+
+function generarAVL() {
+  raiz = null;
+  insertarAVL();
+}
+
+/**
+ * INSERTAR NUEVOS NODOS ARBOL AVL
+ *
+ * Se separan todos los nodos, por medio de la coma
+ * luego son convertidos a int
+ * se valida que el dato ingresado sea un numero
+ * si pasa la validacion, se insertan al arbol
+ */
+
+function insertarAVL() {
+  let input = document.getElementById("inputAVL");
+  array = input.value.split(",");
+
+  for (let i = 0; i < array.length; i++) {
+    let num = parseInt(array[i]);
+    if (!isNaN(num)) {
+      raiz = insertarNodoAVL(raiz, num, true);
+    }
+  }
+  mostrarArbol();
+}
+
+/**
+ * REMOVER NODOS ARBOL AVL
+ *
+ * Se separan todos los nodos, por medio de la coma
+ * luego son convertidos a int
+ * se valida que el dato ingresado sea un numero
+ * si pasa la validacion, se remueven del arbol
+ */
+function eliminarAVL() {
+  let input = document.getElementById("inputAVL");
+  let array = input.value.split(",");
+  for (let i = 0; i < array.length; i++) {
+    let num = parseInt(array[i]);
+    if (!isNaN(num)) {
+      raiz = EliminarNodoAVL(raiz, num, true);
+    }
+  }
+  mostrarArbol();
+}
+
+// -------------ARBOLES ROJO Y NEGRO---------------
+
+/**
+ * CREACION ARBOL ROJO Y NEGRO
+ *
+ * Se define la raiz con altura nula
+ */
+
+function generarRojoYNegro() {
+  raiz = null;
+  insercionRojoYNegro();
+}
+
+/**
+ * INSERTAR NUEVOS NODOS ARBOL ROJO Y NEGRO
+ *
+ * Se separan todos los nodos, por medio de la coma
+ * luego son convertidos a int
+ * se valida que el dato ingresado sea un numero
+ * si pasa la validacion, se insertan al arbol
+ * se define el color de la raiz como negro
+ */
+
+function insercionRojoYNegro() {
+  let input = document.getElementById("inputRN");
+  array = input.value.split(",");
+
+  for (let i = 0; i < array.length; i++) {
+    let num = parseInt(array[i]);
+    if (!isNaN(num)) {
+      raiz = InsertarNodoRojoYNegro(raiz, num);
+      raiz.color = NEGRO;
+    }
+  }
+  mostrarArbol(true);
+}
+
+/**
+ * REMOVER NODOS ARBOL AVL
+ *
+ * Se separan todos los nodos, por medio de la coma
+ * luego son convertidos a int
+ * se valida que el dato ingresado sea un numero
+ * si pasa la validacion se hacen las siguientes
+ * se valida si la raiz es nula o ambos de sus hijos lo som
+ * se define la raiz como nula y se rompe el programa en caso de que si
+ * en caso de que no se remueven del arbol
+ * y se guareda el valor de la raiz
+ */
+
+function eliminarRojoYNegro() {
+  let input = document.getElementById("inputRN");
+  let array = input.value.split(",");
+  for (let i = 0; i < array.length; i++) {
+    let num = parseInt(array[i]);
+    if (!isNaN(num)) {
+      if (
+        raiz == null ||
+        (raiz.hijoIzquierdo == null && raiz.hijoDerecho == null)
+      ) {
+        raiz = null;
+        break;
+      } else {
+        EliminarNodoRojinegro(raiz, num);
+        raiz = obtenerRaiz(raiz);
+      }
+    }
+  }
+  mostrarArbol(true);
+}
+
+// -------------GRAFICAS ---------------
+
+/**
+ * MOSTRAR ARBOLES
+ *
+ * se define la raiz del arbol
+ * se inicializa el lienzo
+ * se limpia y se renderizan los nodos
+ */
+function mostrarArbol(color = false) {
+  medidas(raiz);
+  inicialiizarCanva();
+  limpiar();
+  render(raiz, canvas.width / 2, 10 + radio, color);
+}
+
+//Se definen las variables del canva, controlan el camaño
+var radio = 20;
+var espaciado = 20;
+var altura = radio * 2 + 30;
+var padding = 20;
+var raiz = null;
+var canvas = null;
+var ctx = null;
+
+/***
+ * funcion para inicializar el canva
+ */
+function inicialiizarCanva() {
+  canvas = document.getElementById("canvas");
+  const cvAltura = (raiz.altura - 1) * altura + radio * 2 + padding * 2;
+  const cvAncho = raiz.ancho + padding * 2;
+  canvas.style.height = cvAltura + "px";
+  canvas.style.width = cvAncho + "px";
+  canvas.height = cvAltura;
+  canvas.width = cvAncho;
+  ctx = canvas.getContext("2d");
+  ctx.strokeStyle = "#000";
+  ctx.fillStyle = "#fff";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.font = "bold " + 20 + "px serif";
+}
+
+/***
+ * se define la funcion para vaciar la informacion que tenga el canva
+ * se una una funcion externa para ello, definiendo el tamaño del canva
+ */
+function limpiar() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ *
+ * @param nodo
+ * se define una funcion que maneje las medidas, a la hora de dibujar el arbol
+ * se valida si el nodo es nulo, y en caso de no serlo, se retornan las medidas
+ * se obtiene el ancho de los sub nodos, y se define el nuevo espacio que van a ocupar
+ */
+function medidas(nodo) {
+  if (nodo == null) {
+    return;
+  }
+  if (!nodo.hijoIzquierdo && !nodo.hijoDerecho) {
+    nodo.esListaEnlazada = true;
+    nodo.ancho = radio * 2;
+    return;
+  }
+  medidas(nodo.hijoIzquierdo);
+  medidas(nodo.hijoDerecho);
+
+  // ANCHO SUB ARBOL IZQUIERDO Y DERECHO
+  let anchoIzquierdo = obtenerAncho(nodo.hijoIzquierdo);
+  let anchoDerecho = obtenerAncho(nodo.hijoDerecho);
+
+  let fixNodo = null;
+  if (!nodo.hijoIzquierdo || !nodo.hijoDerecho) {
+    nodo.ancho = anchoIzquierdo + anchoDerecho;
+    nodo.ancho += espaciado;
+    nodo.desplazamiento = espaciado / 2;
+  } else {
+    // SE DEFINE EL NUEVO ESPACIO A OCUPAR
+    let espacioHijos = Math.max(anchoDerecho, anchoIzquierdo);
+    let factor =
+      obtenerAltura(nodo.hijoIzquierdo) - obtenerAltura(nodo.hijoDerecho);
+    nodo.ancho = espacioHijos * 2;
+    nodo.desplazamiento = espacioHijos / 2;
+    let anchoGeneral = Math.abs(anchoIzquierdo - anchoDerecho) / 2;
+    if (factor > 0) {
+      if (anchoIzquierdo >= anchoDerecho) {
+        // SE ACERCAN LOS NODOS PARA REDUCIR ESPACIO
+        let div = Math.pow(2, obtenerAltura(nodo.hijoDerecho));
+        let espacioIzquierdo = anchoIzquierdo / div - radio;
+        if (espacioIzquierdo < 0) {
+          espacioIzquierdo = 0;
+        }
+        anchoGeneral += espacioIzquierdo;
+        fixNodo = nodo.hijoIzquierdo;
+      }
+    } else if (factor < 0) {
+      if (anchoDerecho >= anchoIzquierdo) {
+        let div = Math.pow(2, obtenerAltura(nodo.hijoIzquierdo));
+        let espacioDerecho = anchoDerecho / div - radio;
+        if (espacioDerecho < 0) {
+          espacioDerecho = 0;
+        }
+        anchoGeneral += espacioDerecho;
+        fixNodo = nodo.hijoDerecho;
+      }
+    }
+    nodo.ancho -= anchoGeneral;
+    nodo.desplazamiento -= anchoGeneral / 2;
+    // SE AÑADE EL ESPACIO ENTRE NODOS
+    nodo.ancho += espaciado;
+    nodo.desplazamiento += espaciado / 2;
+    // SE GENERA DISTANCIA ENTRE LOS NODOS HIJOS
+    let distanciahijos = espacioHijos - anchoGeneral + espaciado;
+    if (fixNodo) {
+      let fix = arreglarAncho(fixNodo.desplazamiento, distanciahijos);
+      nodo.ancho += fix;
+      nodo.desplazamiento += fix / 2;
+    }
+  }
+}
+
+/**
+ *
+ * @param desplazamiento
+ * @param distanciahijos
+ * @returns {number}
+ *
+ * se define la funcion para arreglar el ancho a la hora de
+ * balancear el arbol
+ */
+function arreglarAncho(desplazamiento, distanciahijos) {
+  // VERIFICAR SI HAY NODOS O LINEAS SUPERPUESTAS
+  if (Math.atan(altura / desplazamiento) < Math.asin(radio / distanciahijos)) {
+    let sR = radio * radio;
+    let sF = desplazamiento * desplazamiento;
+    let sD = distanciahijos * distanciahijos;
+    let sH = altura * altura;
+    let a = sR - sH;
+    let b = 2 * (sR * desplazamiento - sH * distanciahijos);
+    let c = sH * sR + sR * sF - sH * sD;
+    let res = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let res2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    return res >= 0 ? res : res2;
+  }
+  return 0;
+}
+
+function obtenerAncho(nodo) {
+  return nodo ? nodo.ancho : 0;
+}
+
+/**
+ * SE VERIFICA SI EL SUB-ARBOL ES UNA LISTA ENLAZADA
+ *
+ * se verifica matematicamente si el sub-arbol es una lista enlasada
+ * @param nodo
+ * @returns {boolean}
+ */
+function esListaEnlazada(nodo) {
+  let factor = Math.abs(
+    obtenerAltura(nodo.hijoIzquierdo) - obtenerAltura(nodo.hijoDerecho)
+  );
+  if (factor === nodo.altura - 1) {
+    return true;
+  }
+}
+
+/**
+ * SE DIBUJA EL ARBOL EN PANTALLA
+ *
+ * se dibuja en pantalla en nodo pasandole los parametros de altura
+ * ancho y color(en caso de ser un arbol rojo y negro)
+ * tambien se verifica el desplazamiento del mismo nodo
+ * @param nodo
+ * @param x
+ * @param y
+ * @param color
+ */
+function render(nodo, x, y, color = false) {
+  if (nodo == null) {
+    return;
+  }
+  if (nodo.hijoIzquierdo != null) {
+    let lx = x - nodo.desplazamiento;
+    let ly = y + altura;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(lx, ly);
+    ctx.stroke();
+    render(nodo.hijoIzquierdo, lx, ly, color);
+  }
+  if (nodo.hijoDerecho != null) {
+    let rx = x + nodo.desplazamiento;
+    let ry = y + altura;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(rx, ry);
+    ctx.stroke();
+    render(nodo.hijoDerecho, rx, ry, color);
+  }
+  ctx.beginPath();
+  ctx.arc(x, y, radio, 0, 2 * Math.PI);
+  ctx.closePath();
+  if (color) {
+    ctx.fillStyle = nodo.color === ROJO ? "red" : "black";
+    ctx.fill("nonzero");
+    ctx.fillStyle = "white";
+  } else {
+    ctx.fillStyle = "white";
+    ctx.fill("nonzero");
+    ctx.fillStyle = "black";
+  }
+  ctx.stroke();
+  ctx.fillText(nodo.valor.toString(), x, y);
+}
+
+/**
+ * GENERAR NODOS ALEATORIOS
+ */
+function llenadoAleatorio() {
+  let cantidadNumeros = 50;
+  let array = new Array(cantidadNumeros);
+  let set = new Set();
+  while (set.size < cantidadNumeros) {
+    let num = Math.round(Math.random() * 500 + 1);
+    set.add(num);
+  }
+  let idx = 0;
+  set.forEach((value) => {
+    array[idx] = value;
+    idx++;
+  });
+  return array;
+}
+
+/**
+ * INSERTAR ALEATORIO AVL
+ */
+function llenadoAleatorioAVL() {
+  let input = document.getElementById("inputAVL");
+  input.value = llenadoAleatorio().toString();
+  generarAVL();
+}
+
+/**
+ * INSERTAR ALEATORIO ROJO Y NEGRO
+ */
+function randomRBTree() {
+  let input = document.getElementById("inputRN");
+  input.value = llenadoAleatorio().toString();
+  generarRojoYNegro();
+}
---
+++ Modelo.js
@@ -0,0 +1,521 @@
+const ROJO = 0;
+const NEGRO = 1;
+
+// Se define el nodo, con color rojo por defecto, porque es una hoja
+function Nodo(valor) {
+  this.altura = 1;
+  this.valor = valor;
+  this.hijoIzquierdo = null;
+  this.hijoDerecho = null;
+  this.padre = null;
+  this.color = ROJO;
+  this.ancho = 0;
+  this.desplazamiento = 0;
+}
+
+// Si el nodo existe restorna la altura, delo contrario retorna 0
+function obtenerAltura(nodo) {
+  return nodo ? nodo.altura : 0;
+}
+
+/**
+ * Se obtiene el factor de equilibrio del nodo
+ * Se resta la altura de la rama izquierda con la derecha
+ * @param nodo
+ * @returns {number}
+ */
+function obtenerFactorDeEquilibrio(nodo) {
+  let lh = obtenerAltura(nodo.hijoIzquierdo);
+  let rh = obtenerAltura(nodo.hijoDerecho);
+  return lh - rh;
+}
+
+/**
+ * Se obtiene el nodo mas a la izquierda (el menor)
+ * @param nodo
+ * @returns {*}
+ */
+function obtenerNodoMenor(nodo) {
+  while (nodo.hijoIzquierdo) {
+    nodo = nodo.hijoIzquierdo;
+  }
+  return nodo;
+}
+
+/**
+ * ROTACION A LA DERECHA
+ *
+ * el parametro recibido es el nodo padre
+ * se define la rotacion hacia la derecha para ser usado
+ * despues a la hora de balancear los arboles, toda la rotacion
+ * se hace segun la altura de los nodos
+ * @param nodo
+ * @returns {*}
+ */
+function rotacionDerecha(nodo) {
+  //      P      r
+  //     /      / \
+  //    r      H1  P
+  //   / \        /
+  //  H1 H2      H2
+  // Se realiza el intercambio de nodos
+  let r = nodo.hijoIzquierdo;
+  nodo.hijoIzquierdo = r.hijoDerecho;
+  if (nodo.hijoIzquierdo != null) {
+    nodo.hijoIzquierdo.padre = nodo;
+  }
+  r.hijoDerecho = nodo;
+  r.hijoDerecho.padre = r;
+  r.padre = null;
+  // Se actualiza la altura de los nodos
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+  r.altura =
+    Math.max(obtenerAltura(r.hijoIzquierdo), obtenerAltura(r.hijoDerecho)) + 1;
+  return r;
+}
+
+/**
+ * ROTACION A LA IZQUIERDA
+ *
+ *se define la rotacion hacia la izquierda para ser usado
+ * despues a la hora de balancear los arboles, toda la rotacion
+ * se hace segun la altura de los nodos
+ * @param nodo
+ * @returns {*}
+ */
+function rotacionIzquierda(nodo) {
+  //     P         r
+  //      \       / \
+  //       r     P  H2
+  //      / \     \
+  //     H1 H2     H1
+  // Se realiza el intercambio de nodos
+  let res = nodo.hijoDerecho;
+  nodo.hijoDerecho = res.hijoIzquierdo;
+  if (nodo.hijoDerecho != null) {
+    nodo.hijoDerecho.padre = nodo;
+  }
+  res.hijoIzquierdo = nodo;
+  res.hijoIzquierdo.padre = res;
+  res.padre = null;
+  // Se actualiza la altura de los nodos
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+  res.altura =
+    Math.max(obtenerAltura(res.hijoIzquierdo), obtenerAltura(res.hijoDerecho)) +
+    1;
+  return res;
+}
+
+/**
+ * Balance en arboles AVL
+ * @param nodo
+ * @returns {*}
+ */
+function balancearAVL(nodo) {
+  // Caso 1
+  //              P
+  //            /
+  //           H
+  //         /
+  //       N
+  if (
+    obtenerFactorDeEquilibrio(nodo) > 1 &&
+    obtenerFactorDeEquilibrio(nodo.hijoIzquierdo) >= 0
+  ) {
+    nodo = rotacionDerecha(nodo);
+  }
+  // Caso 2
+  //     P
+  //      \
+  //       H
+  //        \
+  //         N
+  if (
+    obtenerFactorDeEquilibrio(nodo) < -1 &&
+    obtenerFactorDeEquilibrio(nodo.hijoDerecho) <= 0
+  ) {
+    nodo = rotacionIzquierda(nodo);
+  }
+  // Caso 3
+  //     P
+  //    /
+  //  H
+  //   \
+  //    N
+  if (
+    obtenerFactorDeEquilibrio(nodo) > 1 &&
+    obtenerFactorDeEquilibrio(nodo.hijoIzquierdo) < 0
+  ) {
+    nodo.hijoIzquierdo = rotacionIzquierda(nodo.hijoIzquierdo);
+    nodo = rotacionDerecha(nodo);
+  }
+  // Caso 4
+  //     P
+  //      \
+  //       H
+  //     /
+  //   N
+  if (
+    obtenerFactorDeEquilibrio(nodo) < -1 &&
+    obtenerFactorDeEquilibrio(nodo.hijoDerecho) > 0
+  ) {
+    nodo.hijoDerecho = rotacionDerecha(nodo.hijoDerecho);
+    nodo = rotacionIzquierda(nodo);
+  }
+  return nodo;
+}
+
+/**
+ * Insercion de un nodo AVL
+ * @param nodo raiz donde se va a insertar
+ * @param valor valor del nodo a insertar
+ * @returns {Nodo|*}
+ */
+function insertarNodoAVL(nodo, valor) {
+  // Retorna un nuevo nodo si el arbol esta vacio
+  if (nodo == null) {
+    return new Nodo(valor);
+  }
+  // Inserta el nodo de manera recursiva, hasta llegar a la hoja
+  if (valor < nodo.valor) {
+    nodo.hijoIzquierdo = insertarNodoAVL(nodo.hijoIzquierdo, valor);
+  } else if (valor >= nodo.valor) {
+    nodo.hijoDerecho = insertarNodoAVL(nodo.hijoDerecho, valor);
+  }
+  // Recalcula altura, recursivamente de abajo hacia arriba
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+  // balancea el arbol si es necesario
+
+  nodo = balancearAVL(nodo);
+
+  return nodo;
+}
+
+/**
+ * Eliminacion de un nodo
+ * @param nodo raiz de don de se busca eliminar
+ * @param valor valor del nodo a eliminar
+ * @returns {Nodo|*}
+ */
+function EliminarNodoAVL(nodo, valor) {
+  // Retorna null si el arbol esta vacio
+  if (nodo == null) {
+    return null;
+  }
+  // Se busca el nodo a eliminar de manera recursiva
+  if (valor < nodo.valor) {
+    nodo.hijoIzquierdo = EliminarNodoAVL(nodo.hijoIzquierdo, valor);
+  } else if (valor > nodo.valor) {
+    nodo.hijoDerecho = EliminarNodoAVL(nodo.hijoDerecho, valor);
+  } else {
+    // El nodo actual es el nodo a eliminar
+    if (nodo.hijoIzquierdo == null && nodo.hijoDerecho == null) {
+      return null;
+    } else if (nodo.hijoIzquierdo == null || nodo.hijoDerecho == null) {
+      return nodo.hijoIzquierdo == null ? nodo.hijoDerecho : nodo.hijoIzquierdo;
+    } else {
+      // Se busca el reemplazo (el mas a la izquierda de la rama derecha)
+      let min = obtenerNodoMenor(nodo.hijoDerecho);
+      // Se elimina el reemplazo
+      nodo.hijoDerecho = EliminarNodoAVL(nodo.hijoDerecho, min.valor);
+      nodo.valor = min.valor;
+    }
+  }
+  // Se recalcula la altura de los nodos
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+  // Se balancea el arbol si es necesario
+  nodo = balancearAVL(nodo);
+
+  return nodo;
+}
+
+function esRojo(nodo) {
+  if (nodo == null) return false;
+  return nodo.color === ROJO;
+}
+
+function obtenerRaiz(nodo) {
+  if (nodo.padre) {
+    return nodo.padre;
+  }
+  return nodo;
+}
+
+/**
+ * INSERTAR NODO ARBOL ROJO Y NEGRO
+ * @param nodo raiz donde se busca
+ * @param valor valor a elimina
+ * @returns {Nodo|*}
+ * @constructor
+ */
+function InsertarNodoRojoYNegro(nodo, valor) {
+  // Retorna un nuevo nodo si el arbol esta vacio
+  if (nodo == null) {
+    return new Nodo(valor);
+  }
+  // Inserta el nodo de manera recursiva, hasta llegar a la hoja
+  if (valor < nodo.valor) {
+    nodo.hijoIzquierdo = InsertarNodoRojoYNegro(nodo.hijoIzquierdo, valor);
+    nodo.hijoIzquierdo.padre = nodo;
+  } else if (valor >= nodo.valor) {
+    nodo.hijoDerecho = InsertarNodoRojoYNegro(nodo.hijoDerecho, valor);
+    nodo.hijoDerecho.padre = nodo;
+  }
+  // Calcula la altura del nodo
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+  // Se usa el nodo como abuelo y se verifica si se necesita equilibrar
+  if (esRojo(nodo.hijoIzquierdo) && esRojo(nodo.hijoDerecho)) {
+    // Caso 1 (cambio de color)
+    // Si el nodo padre del nodo insertado es de color rojo, y el hermano del nodo padre también es de color rojo,
+    // es decir, abuelo->padre->nodo insertado = negro->rojo->rojo, se puede cambiar el color de las tres capas a rojo->negro->rojo.
+    // Se comprueba el caso simetrico
+    if (
+      esRojo(nodo.hijoIzquierdo.hijoIzquierdo) ||
+      esRojo(nodo.hijoIzquierdo.hijoDerecho) ||
+      esRojo(nodo.hijoDerecho.hijoDerecho) ||
+      esRojo(nodo.hijoDerecho.hijoIzquierdo)
+    ) {
+      nodo.hijoIzquierdo.color = NEGRO;
+      nodo.hijoDerecho.color = NEGRO;
+      nodo.color = ROJO;
+    }
+  } else if (esRojo(nodo.hijoIzquierdo)) {
+    // Caso 3 (Tio negro, nodo x es hijo izquierdo)
+    // El nodo padre del nodo insertado es de color rojo,
+    // y el hermano del nodo padre es de color negro
+    if (esRojo(nodo.hijoIzquierdo.hijoIzquierdo)) {
+      // Rotar hacia la derecha el nodo abuelo
+      nodo.color = ROJO;
+      nodo.hijoIzquierdo.color = NEGRO;
+      nodo = rotacionDerecha(nodo);
+    } else if (esRojo(nodo.hijoIzquierdo.hijoDerecho)) {
+      //(Caso 2)
+      // Rotar a la izquierda y luego a la derecha
+      nodo.hijoIzquierdo = rotacionIzquierda(nodo.hijoIzquierdo);
+      nodo.color = ROJO;
+      nodo.hijoIzquierdo.color = NEGRO;
+      nodo = rotacionDerecha(nodo);
+    }
+  }
+  // (Caso simetrico)
+  else if (esRojo(nodo.hijoDerecho)) {
+    if (esRojo(nodo.hijoDerecho.hijoDerecho)) {
+      // Rotar hacia la derecha el nodo abuelo
+      nodo.color = ROJO;
+      nodo.hijoDerecho.color = NEGRO;
+      nodo = rotacionIzquierda(nodo);
+    } else if (esRojo(nodo.hijoDerecho.hijoIzquierdo)) {
+      // Rotar a la izquierda y luego a la derecha
+      nodo.hijoDerecho = rotacionDerecha(nodo.hijoDerecho);
+      nodo.color = ROJO;
+      nodo.hijoDerecho.color = NEGRO;
+      nodo = rotacionIzquierda(nodo);
+    }
+  }
+  return nodo;
+}
+
+/**
+ * ELIMINACION DE NODOS ROJO Y NEGRO
+ * @param nodo raiz donde se elimina
+ * @param valor valor a eliminar
+ * @returns {null|*}
+ * @constructor
+ */
+function EliminarNodoRojinegro(nodo, valor) {
+  // Si el arbol esta vacio no se hace nada
+  if (nodo == null) {
+    return;
+  }
+  if (valor < nodo.valor) {
+    EliminarNodoRojinegro(nodo.hijoIzquierdo, valor);
+  } else if (valor > nodo.valor) {
+    EliminarNodoRojinegro(nodo.hijoDerecho, valor);
+  } else {
+    if (nodo.hijoIzquierdo == null && nodo.hijoDerecho == null) {
+      // CASO #1 eliminar el nodo hoja
+      arreglarNodoRojinegro(nodo);
+      // Eliminar el nodo despues de haber arreglado el arbol
+      if (nodo === nodo.padre.hijoIzquierdo) {
+        nodo.padre.hijoIzquierdo = null;
+      } else {
+        nodo.padre.hijoDerecho = null;
+      }
+      nodo.padre = null;
+    } else if (nodo.hijoIzquierdo == null || nodo.hijoDerecho == null) {
+      // CASO #2 Eliminar el nodo que tiene un solo hijo
+      //       -
+      //     /    \
+      //eliminado  -
+      //   /      / \
+      //  H      -   -
+      // Eliminar el nodo y remplazarlo por su hijo
+      let hijo =
+        nodo.hijoIzquierdo == null ? nodo.hijoDerecho : nodo.hijoIzquierdo;
+      nodo.valor = hijo.valor;
+      // Cambiar a caso #1
+      EliminarNodoRojinegro(hijo, hijo.valor);
+    } else {
+      // CASO #3 Encontrar el sucesor (El nodo mas a la izquierda de la rama derecha)
+      let min = obtenerNodoMenor(nodo.hijoDerecho);
+      nodo.valor = min.valor;
+      // Se convierte en caso #1 o #2
+      EliminarNodoRojinegro(min, min.valor);
+    }
+  }
+  // Se actualiza la altura del nodo
+  nodo.altura =
+    Math.max(
+      obtenerAltura(nodo.hijoIzquierdo),
+      obtenerAltura(nodo.hijoDerecho)
+    ) + 1;
+}
+
+/**
+ * Arreglos que se deben hacer despues de eliminar un nodo
+ * @param nodo
+ */
+function arreglarNodoRojinegro(nodo) {
+  while (nodo.padre != null && nodo.color === NEGRO) {
+    let padre = nodo.padre;
+    let abuelo = padre.padre;
+    let hermano;
+    if (nodo.valor < padre.valor) {
+      //Eliminar el nodo que es el hijo izquierdo de su nodo padre
+      hermano = padre.hijoDerecho;
+      if (!esRojo(hermano)) {
+        //El hermano del nodo a eliminar es de color negro
+        if (esRojo(hermano.hijoDerecho)) {
+          //Caso(4)
+          // CASO #1 El hijo derecho del hermano del nodo a eliminar es de color rojo, mientras que el hijo izquierdo puede tener cualquier color. Es decir, se cumple la condición ++
+          // Debido a la eliminación de un nodo negro en el subárbol izquierdo, simplemente toma un nodo rojo del subárbol derecho y lo cambia a negro
+          //       R/N             N            R/N           R/N
+          //       / \           /  \          /  \           / \
+          //    elim  N   =>  elim  R/N   =>  N   N   =>    N    N
+          //        /  \            / \      / \             \
+          //       -   R          -    N   elim  -             -
+          hermano.color = padre.color;
+          //Transfiere el color del nodo padre al hermano. Tanto el nodo padre como el hijo derecho del hermano se vuelven negros, luego se realiza una rotación hacia la izquierda
+          padre.color = NEGRO;
+          hermano.hijoDerecho.color = NEGRO;
+          padre = rotacionIzquierda(padre);
+          enlazarPadre(padre, abuelo);
+          break;
+        } else if (
+          !esRojo(hermano.hijoDerecho) &&
+          esRojo(hermano.hijoIzquierdo)
+        ) {
+          // CASO #2 El hijo izquierdo del hermano del nodo a eliminar es de color rojo, mientras que el hijo derecho es de color negro o nulo. Es decir, se cumple la condición +-
+          //     R/N         R/N                   R/N
+          //     / \          / \     Rotacion     / \
+          //  elim  N  =>  elim  R    derecha   elim  N   =>  Se convierte en el caso 1 ++
+          //      /             /       =>             \
+          //     R            N                         R
+          hermano.hijoIzquierdo.color = NEGRO;
+          hermano.color = ROJO;
+          hermano = rotacionDerecha(hermano);
+          enlazarPadre(hermano, padre);
+        } else if (
+          !esRojo(hermano.hijoIzquierdo) &&
+          !esRojo(hermano.hijoDerecho)
+        ) {
+          // CASO #3 El hermano del nodo no tiene hijos o todos sus hijos son de color negro
+          //      R/N          R/N  <- Se vuelve a verificar el arbol desde aqui
+          //     /  \    =>    / \
+          //  elim   N      elim  R
+          //        / \          / \
+          //       N  N        N    N
+          hermano.color = ROJO;
+          nodo = padre;
+        }
+      } else {
+        // CASO #4 Si el hermano del nodo es de color rojo, entonces el nodo padre es de color negro y el hermano tiene dos hijos negros
+        //      N               R                   N
+        //     / \            /  \   Rotacion      / \
+        //  elim  R   =>   elim  N  izquierda     R  N   =>  Se convierte en caso 1/2/3
+        //      / \        / \         =>        /\
+        //     N  N       N  N               elim  N
+        hermano.color = NEGRO;
+        padre.color = ROJO;
+        padre = rotacionIzquierda(padre);
+        enlazarPadre(padre, abuelo);
+      }
+    } else {
+      // Eliminar el nodo que es el hijo derecho de su nodo padre
+      hermano = padre.hijoIzquierdo;
+      if (!esRojo(hermano)) {
+        // El hermano del nodo a eliminar es de color negro
+        if (esRojo(hermano.hijoIzquierdo)) {
+          // CASO #5 El hijo izquierdo del hermano del nodo a eliminar es de color rojo, mientras que el hijo derecho puede tener cualquier color. Es decir, se cumple la condición --
+          hermano.color = padre.color;
+          // Transfiere el color del nodo padre al hermano. Tanto el nodo padre como el hijo derecho del hermano se vuelven negros, luego se realiza una rotación hacia la izquierda
+          padre.color = NEGRO;
+          hermano.hijoIzquierdo.color = NEGRO;
+          padre = rotacionDerecha(padre);
+          enlazarPadre(padre, abuelo);
+          break;
+        } else if (
+          !esRojo(hermano.hijoIzquierdo) &&
+          esRojo(hermano.hijoDerecho)
+        ) {
+          // CASO #6 El hijo derecho del hermano del nodo a eliminar es de color rojo, mientras que el hijo izquierdo es de color negro o nulo. Es decir, se cumple la condición -+
+          hermano.hijoDerecho.color = NEGRO;
+          hermano.color = ROJO;
+          hermano = rotacionIzquierda(hermano);
+          enlazarPadre(hermano, padre);
+        } else if (
+          !esRojo(hermano.hijoIzquierdo) &&
+          !esRojo(hermano.hijoDerecho)
+        ) {
+          // CASO #7 El hermano del nodo a eliminar no tiene hijos o todos sus hijos son de color negro
+          hermano.color = ROJO;
+          nodo = padre;
+        }
+      } else {
+        // CASO #8 Si el hermano del nodo es de color rojo, entonces el nodo padre es de color negro y el hermano tiene dos hijos negros
+        hermano.color = NEGRO;
+        padre.color = ROJO;
+        padre = rotacionDerecha(padre);
+        enlazarPadre(padre, abuelo);
+      }
+    }
+  }
+  //Se cambia el color al final
+  nodo.color = NEGRO;
+}
+
+/**
+ * Se enlazan el nuevo hijo y padre, se verifica si sera hijo derecho o izquierdo
+ * @param node nuevo hijo
+ * @param padre
+ */
+function enlazarPadre(nodo, padre) {
+  if (padre) {
+    if (nodo.valor < padre.valor) {
+      padre.hijoIzquierdo = nodo;
+    } else {
+      padre.hijoDerecho = nodo;
+    }
+    nodo.padre = padre;
+  }
+}
